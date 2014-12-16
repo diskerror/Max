24774a7d04932bf1d9af5a912d88ea13bfa31caa
Reid Woodbury Jr. <diskerrer@gmail.com>
2014-12-16 14:52:20 -0800

    many, many cleanups

--- iir~.c
+++ iir~.c
@@ -25,6 +25,9 @@
 
 void *iir_class;
 
+#define IIR_MAX_POLES		20
+#define IIR_COEF_MEM_SIZE	((IIR_MAX_POLES+1) * sizeof(double))
+
 typedef struct
 {
 	t_pxobject l_obj;
@@ -47,7 +50,6 @@ t_int *iir_perform(t_int *w);
 void iir_perform64(t_iir *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
 void iir_clear(t_iir *x);
 void iir_coeffs(t_iir *x, t_symbol *, short argc, t_atom *argv);
-void iir_releasePtrs(t_iir *x);
 
 int C74_EXPORT main(void)
 {
@@ -74,6 +76,7 @@ int C74_EXPORT main(void)
 void *iir_new(t_symbol *o, long argc, t_atom *argv)
 {
     t_iir *x = NULL;
+    long c;
     
     if( (x = (t_iir *)object_alloc(iir_class)) )
     {
@@ -83,16 +86,27 @@ void *iir_new(t_symbol *o, long argc, t_atom *argv)
 		outlet_new((t_object *)x, "signal");
 	
 		//	post message
-		post("iir~ [aabab|aaabb]");
+		object_post((t_object *)x, "iir~ [aabab|aaabb]");
 	
 		//	output order
 		if( argc && !strcmp(atom_getsym(argv)->s_name, "aaabb") )
 			x->inOrder = 1;
 		else
 			x->inOrder = 0;
-
-		//	start with all pointers == null
-		x->a = x->b = x->x = x->y = NULL;
+		
+		//	now we need pointers for our new data
+		x->a = (double *)getbytes(IIR_COEF_MEM_SIZE);	//	input coefs
+		x->b = (double *)getbytes(IIR_COEF_MEM_SIZE);	//	output coefs
+		x->x = (double *)getbytes(IIR_COEF_MEM_SIZE);	//	delayed input
+		x->y = (double *)getbytes(IIR_COEF_MEM_SIZE);	//	delayed output
+		
+		for ( c=0; c<=IIR_MAX_POLES; c++ )
+			x->x[c] = x->y[c] = 0.0;
+		
+		if (!x->a || !x->b || !x->x || !x->y)
+			object_error((t_object *)x, "BAD INIT POINTER");
+		
+		x->poles = 0.0;
 	}
 
     return (x);
@@ -101,7 +115,11 @@ void *iir_new(t_symbol *o, long argc, t_atom *argv)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void iir_free(t_iir *x)
 {
-	iir_releasePtrs(x);
+	if(x->a) freebytes(x->a, IIR_COEF_MEM_SIZE);
+	if(x->b) freebytes(x->b, IIR_COEF_MEM_SIZE);
+	if(x->x) freebytes(x->x, IIR_COEF_MEM_SIZE);
+	if(x->y) freebytes(x->y, IIR_COEF_MEM_SIZE);
+	
 	dsp_free((t_pxobject *)x);
 }
 
@@ -111,9 +129,7 @@ void iir_assist(t_iir *x, void *b, long m, long a, char *s)
 	if (m == 2)
 		sprintf(s,"(signal) Output");
 	else
-	{
 		sprintf(s,"(signal) Input, List Input");
-	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -134,12 +150,12 @@ void iir_print(t_iir *x)
 	long i;
 	if (x->a)
 	{
-		post("a[00] = % .15e", x->a[0]);
+		object_post((t_object *)x, "a[00] = % .15e", x->a[0]);
 		for ( i=1; i <= x->poles; i++ )
-			post("a[%02d] = % .15e   b[%02d] = % .15e", i, x->a[i], i, x->b[i]);
+			object_post((t_object *)x, "a[%02d] = % .15e   b[%02d] = % .15e", i, x->a[i], i, x->b[i]);
 	}
 	else
-		post("a[00] = 0.0");
+		object_post((t_object *)x, "a[00] = 0.0");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -162,6 +178,7 @@ t_int *iir_perform(t_int *w)
 	// assign from parameters
     t_float *in = (t_float *) w[1];
     t_float *out = (t_float *) w[2];
+    register t_double y0;	//	de-referenced "x->y[0]"
     t_iir *x = (t_iir *) w[3];
     long n = (long) w[4];
     register long p;
@@ -170,30 +187,25 @@ t_int *iir_perform(t_int *w)
 		return (w+5);
 	
 	// DSP loops
-	if (x->a && x->b && x->x && x->y)
-	{
-		while (n--)
-		{
+	if (x->poles && x->a && x->b && x->x && x->y) {
+		while (n--) {
 			//	delay old values one sample
-			for (p=x->poles; p>0; p--)
-			{
+			for (p=x->poles; p>0; p--) {
 				x->x[p] = x->x[p-1];
 				x->y[p] = x->y[p-1];
 			}
 			x->x[0] = (double)*in++;
 			
-			x->y[0] = x->x[0] * x->a[0];
-			for (p=1; p<=x->poles; p++)
-			{
-				x->y[0] += x->x[p] * x->a[p];
-				x->y[0] += x->y[p] * x->b[p];
+			y0 = x->x[0] * x->a[0];
+			for (p=1; p<=x->poles; p++) {
+				y0 += x->x[p] * x->a[p];
+				y0 += x->y[p] * x->b[p];
 			}
 			
-			*out++ = (float)x->y[0];
+			*out++ = x->y[0] = (t_float)y0;
 		}
 	}
-	else	//	if pointers are no good...
-	{
+	else {	//	if pointers are no good...
 		while (n--)
 			*out++ = *in++;	//	...just copy input to output
 	}
@@ -206,6 +218,7 @@ void iir_perform64(t_iir *x, t_object *dsp64, double **ins, long numins, double 
 {
     t_double *in = ins[0];
     t_double *out = outs[0];
+    register t_double y0;	//	de-referenced "x->y[0]"
     long n = sampleframes;
     register long p;
     
@@ -213,30 +226,25 @@ void iir_perform64(t_iir *x, t_object *dsp64, double **ins, long numins, double 
 		return;
 	
 	// DSP loops
-	if (x->a && x->b && x->x && x->y)
-	{
-		while (n--)
-		{
+	if (x->poles && x->a && x->b && x->x && x->y) {
+		while (n--) {
 			//	delay old values one sample
-			for (p=x->poles; p>0; p--)
-			{
+			for (p=x->poles; p>0; p--) {
 				x->x[p] = x->x[p-1];
 				x->y[p] = x->y[p-1];
 			}
 			x->x[0] = *in++;
 			
-			x->y[0] = x->x[0] * x->a[0];
-			for (p=1; p<=x->poles; p++)
-			{
-				x->y[0] += x->x[p] * x->a[p];
-				x->y[0] += x->y[p] * x->b[p];
+			y0 = x->x[0] * x->a[0];
+			for (p=1; p<=x->poles; p++) {
+				y0 += x->x[p] * x->a[p];
+				y0 += x->y[p] * x->b[p];
 			}
 			
-			*out++ 	= x->y[0];
+			*out++ = x->y[0] = y0;
 		}
 	}
-	else	//	if pointers are no good...
-	{
+	else {	//	if pointers are no good...
 		while (n--)
 			*out++ = *in++;	//	...just copy input to output
 	}
@@ -246,75 +254,49 @@ void iir_perform64(t_iir *x, t_object *dsp64, double **ins, long numins, double 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void iir_clear(t_iir *x)
 {
-	long p;
-	for ( p=1; p<=x->poles; p++ )
-		x->y[p] = 0.0;	//	y[0] is not used
+	long c;
+	for ( c=0; c<=IIR_MAX_POLES; c++ )
+		x->y[c] = 0.0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void iir_coeffs	(t_iir *x, t_symbol *s, short argc, t_atom *argv)
 {
-	long i, p;
+	long i, p, poles;
 	
-	for(i=0; i<argc; i++)
-	{
-		if (argv[i].a_type != A_FLOAT)
-		{
-			post("WARNING: All list members must be of type float or double.");
+	for(i=0; i<argc; i++) {
+		if (argv[i].a_type != A_FLOAT) {
+			object_post((t_object *)x, "WARNING: All list members must be of type float or double.");
+			x->poles = 0.0;
 			return;
 		}
 	}
 	
-	iir_releasePtrs(x);	//	does nothing if pointer are null
-	x->poles = argc/2;	//	integer division
-	
-	//	now we need pointers for our new data
-	x->a	= (double *)getbytes((x->poles+1) * sizeof(double));
-	x->b	= (double *)getbytes((x->poles+1) * sizeof(double));
-	x->x	= (double *)getbytes((x->poles+1) * sizeof(double));
-	x->y	= (double *)getbytes((x->poles+1) * sizeof(double));
+	poles = argc/2;	//	integer division
 	
 	//	clear the x and y space. Make them start as silence.
-	for ( p=0; p<=x->poles; p++ )
-		x->x[p] = x->y[p] = 0.0;
+// 	for ( p=0; p<=poles; p++ )
+// 		x->x[p] = x->y[p] = 0.0;
 
 	x->a[0] = argv[0].a_w.w_float;	//	the first is always the same no matter the order
-	if (x->inOrder)	//	aaabb
-	{
-		for (p=1; p<=x->poles; p++)
-		{
+	if (x->inOrder) {	//	aaabb
+		for (p=1; p<=poles && p<=IIR_MAX_POLES; p++) {
 			x->a[p] = (double)argv[p].a_w.w_float;
-			x->b[p] = (double)argv[x->poles+p].a_w.w_float;
+			x->b[p] = (double)argv[poles+p].a_w.w_float;
 		}
 	}
-	else				//	aabab
-	{
-		for (p=1; p<=x->poles; p++)
-		{
+	else {				//	aabab
+		for (p=1; p<=poles && p<=IIR_MAX_POLES; p++) {
 			x->a[p] = (double)argv[p*2-1].a_w.w_float;
 			x->b[p] = (double)argv[p*2].a_w.w_float;
 		}
 	}
 	
-	if (!(argc % 2))	//	if the list is short...
-		x->b[x->poles] = 0.0;	//	...then there's bogus data in the last b
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void iir_releasePtrs(t_iir *x)
-{
-	if (x->a && x->b && x->x && x->y)	//	all must != zero
-	{
-		freebytes(x->a, (x->poles+1) * sizeof(double));
-		freebytes(x->b, (x->poles+1) * sizeof(double));
-		freebytes(x->x, (x->poles+1) * sizeof(double));
-		freebytes(x->y, (x->poles+1) * sizeof(double));
-		
-		x->a = x->b = x->x = x->y = 0L;
+	if( poles > IIR_MAX_POLES )
+		x->poles = IIR_MAX_POLES;
+	else {
+		x->poles = poles;
+		if (!(argc % 2))	//	if the list is short...
+			x->b[poles] = 0.0;	//	...then there's bogus data in the last b
 	}
-	//else
-	//	error("iir_releasePtrs; one pointer was already zero.");
-	
-	//not an error
-	//	assumes pointers were never allocated
 }
